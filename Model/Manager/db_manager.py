e4a7319768e4da99b515b5ce2652836009070ae0
Mica <migatsar@gmail.com>
2020-06-19 15:57:53 +0200

    Creation of first version of DBManager

--- Model/Manager/db_manager.py
+++ Model/Manager/db_manager.py
@@ -10,19 +10,17 @@ Every coding elements with SQL should be regrouped in the ORM.
 """
 
 
-import os
 import mysql.connector as mysql
 from mysql.connector import Error
 
-import Static.credential as credential  # module with private settings
 import Static.constant as constant
 
 
 class Borg:
     """
     Metaclass to apply the Borg pattern to a subclass.
-    It makes sure that there is only one instance of a class that is in use.
 
+    It makes sure that there is only one instance of a class that is in use.
     The Borg pattern is an alternative to the Singleton.
     It doesn't really block the creation of over instances,
     but it ensures that all instances share state and behavior.
@@ -37,15 +35,12 @@ class Borg:
         self.__dict__ = self.__shared_state
 
 
-class DBConnector(Borg):
-    """
-    Initiate the connection to MySQL server or a database,
-    and create an object to interact with it.
-    """
+class DBConnection(Borg):
+    """ Initiate the connection to MySQL server or a database """
 
     def __init__(self, host_name, user_name, user_password, db_name=None):
 
-        # ensure that only one instance of DBConnector is at play
+        # ensure that only one instance of DBConnection is at play
         Borg.__init__(self)
 
         self.host_name = host_name
@@ -54,16 +49,15 @@ class DBConnector(Borg):
         self.db_name = db_name
 
         # initiate the connection
-        self.connection = self.set_connection()
-        self.cursor = self.set_cursor()
+        self.connection = self.create_connection()
 
-    def set_connection(self):
+    def create_connection(self):
         """
         Create a connection with MySQL server,
         if db_name is specified, create a connection specifically to a database
         """
 
-        if self.db_name is None :
+        if self.db_name is None:
             # create a general connection to MySQL server
 
             try:
@@ -82,66 +76,49 @@ class DBConnector(Borg):
 
             try:
                 self.connection = mysql.connect(
-                    host=self.host_name,
-                    user=self.user_name,
-                    passwd=self.user_password,
-                    database=self.db_name
+                        host=self.host_name,
+                        user=self.user_name,
+                        passwd=self.user_password,
+                        database=self.db_name
                 )
             except Error as e:
                 print(f"The error'{e}' occured")
 
             return self.connection
 
-    def set_cursor(self):
-        """
-        Return an instance of cursor.
-        The cursor is the object that interact with the DB server.
-        It execute operations such as SQL statements.
-        """
-
-        if self.connection is None:
-            print("No connection on DB to instantiate the cursor ")
-        else:
-            self.cursor = self.connection.cursor()
-
-        return self.cursor
-
 
 class DBManager:
-    """
-    Initiate the interaction with the database, if it already exists.
-    Create the database otherwise.
-    """
+    """Create the database or initiate its connection, if it already exists."""
 
     def __init__(self, host_name, user_name, user_password, db_name):
-
         self.host_name = host_name
         self.user_name = user_name
         self.user_password = user_password
         self.db_name = db_name
 
         # initiate a connection to the server
-        self._connector = self.get_connector()
-        self.connection = self._connector.connection
-        self.cursor = self._connector.cursor
+        self.connection = self.get_connection()
+        self.cursor = self.connection.cursor()
 
-    def get_connector(self, db_name=None):
+    def get_connection(self, db_name=None):
         """
+        Retrieve a connection.
+
         Connect with a database if db_name is specified, otherwise connect with the server
-        It returns the cursor to the connection as well
         """
 
-        self._connector = DBConnector(self.host_name, self.user_name, self.user_password, db_name)
+        _cnx = DBConnection(self.host_name, self.user_name, self.user_password, db_name)
 
-        return self._connector
+        return _cnx.connection
 
     def build_database(self, filepath=constant.SCHEMA_PATH):
         """
         Initiate the creation of the database.
+
         Take the path to a sql file as variable, and use the file as a schema to build the database
         """
 
-        # open the sql file with as an object to pass its content to other methods
+        # open the sql file as an object to pass its content to other methods
         with open(filepath, 'r') as f:
             sql_file = f.read()
             # methods to format the content of the sql file
@@ -154,9 +131,7 @@ class DBManager:
         for line in sql_file:
             self.cursor.execute(line)
 
-        # renew _connector to initiate a connection with the database
-        self._connector = self.get_connector(self.db_name)
-        self.connection = self._connector.connection
-        self.cursor = self._connector.cursor
-
+        # renew get_connection() to initiate a connection with the database
+        self.connection = self.get_connection(self.db_name)
+        self.cursor = self.connection.cursor()
         return self.connection, self.cursor
