ca4e2d75a143a852ec99f38427f2986673134fea
Mica <migatsar@gmail.com>
2020-06-19 16:03:42 +0200

    Simplification of the sql schema to build the database

--- Static/schema_purbeurre.sql
+++ Static/schema_purbeurre.sql
@@ -11,11 +11,12 @@ DROP TABLE IF EXISTS `product` ;
 CREATE TABLE IF NOT EXISTS `product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(250) DEFAULT NULL,
-  `score` int(11) DEFAULT NULL,
+  `nutriscore` int(11) DEFAULT NULL,
+  `code` bigint DEFAULT NULL,
   `url` varchar(500) DEFAULT NULL,
-  `description` longtext,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 DROP TABLE IF EXISTS `category` ;
 CREATE TABLE IF NOT EXISTS `category` (
@@ -24,6 +25,7 @@ CREATE TABLE IF NOT EXISTS `category` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
 DROP TABLE IF EXISTS `store` ;
 CREATE TABLE IF NOT EXISTS `store` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -31,8 +33,9 @@ CREATE TABLE IF NOT EXISTS `store` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `favorite` ;
-CREATE TABLE IF NOT EXISTS `favorite` (
+
+DROP TABLE IF EXISTS `favorite_product` ;
+CREATE TABLE IF NOT EXISTS `favorite_product` (
   `base_product_id` int(11) NOT NULL,
   `substitute_product_id` int(11) NOT NULL,
   PRIMARY KEY (`base_product_id`,`substitute_product_id`),
@@ -42,24 +45,26 @@ CREATE TABLE IF NOT EXISTS `favorite` (
   CONSTRAINT `fk_favorite_product2` FOREIGN KEY (`substitute_product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `product_category` ;
-CREATE TABLE IF NOT EXISTS `product_category` (
-  `product_id` int(11) NOT NULL,
+
+DROP TABLE IF EXISTS `category_product` ;
+CREATE TABLE IF NOT EXISTS `category_product` (
   `category_id` int(11) NOT NULL,
-  PRIMARY KEY (`product_id`,`category_id`),
-  KEY `fk_product_category_product1_idx` (`product_id`),
-  KEY `fk_product_category_category_idx` (`category_id`),
-  CONSTRAINT `fk_product_category_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_product_category_product1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`category_id`, `product_id`),
+  KEY `fk_category_product_category_idx` (`category_id`),
+  KEY `fk_category_product_product_idx` (`product_id`),
+  CONSTRAINT `fk_category_product_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_category_product_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `product_store` ;
-CREATE TABLE IF NOT EXISTS `product_store` (
-  `product_id` int(11) NOT NULL,
+
+DROP TABLE IF EXISTS `store_product` ;
+CREATE TABLE IF NOT EXISTS `store_product` (
   `store_id` int(11) NOT NULL,
-  PRIMARY KEY (`product_id`,`store_id`),
-  KEY `fk_product_store_product1_idx` (`product_id`),
-  KEY `fk_product_store_store1_idx` (`store_id`),
-  CONSTRAINT `fk_product_store_product1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_product_store_store1` FOREIGN KEY (`store_id`) REFERENCES `store` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`store_id`, `product_id`),
+  KEY `fk_store_product_store_idx` (`store_id`),
+  KEY `fk_store_product_product_idx` (`product_id`),
+  CONSTRAINT `fk_store_product_store` FOREIGN KEY (`store_id`) REFERENCES `store` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_store_product_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
