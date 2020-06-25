a4993cfeb45f5a7f2fabfb78d276256c597c6dca
Mica <migatsar@gmail.com>
2020-06-25 15:42:52 +0200

    added methods to list related categories and stores

--- Model/Entity/product.py
+++ Model/Entity/product.py
@@ -1,40 +1,65 @@
 """ Create an object 'Product' to carry the data of the products. """
 
 
+from Model.Entity.category import Category
+from Model.Entity.store import Store
+
+
 class Product:
     """Create an object describing one product"""
 
-    def __init__(self, name, nutriscore, code, categories, stores, url, id=None):
+    def __init__(self, name, brands, nutriscore, code, url, categories=None, stores=None, id=None):
 
         self.id = id
         self.name = name
+        self.brands = brands
 
         self.nutriscore = nutriscore
         self.code = code
         self.url = url
-        self.categories = []
+
+        self.categories = categories
         # for cat in categories.split(',')
         #   try:
         #       cat = Categorie(name)
         #       ajouter la categorie à self.categories
 
-        # self.categories = categories
-        # self.stores = stores
+        self.stores = stores
+
+    @staticmethod
+    def get_categories(categories, product=None):
+        """Create a list of instances of 'Category' and append the list to 'self.categories'."""
+
+        cats = []
+        for cat in categories.split(','):
+            try:
+                category = Category(
+                        name=str(cat).strip(),
+                        products=product
+                )
+                cats.append(category)
+            except KeyError:
+                continue
+        return cats
+
+    @staticmethod
+    def get_stores(stores, product=None):
+        """Create a list of instances of 'Store' and append the list to 'self.stores'."""
+
+        shops = []
+        for shop in stores.split(','):
+            try:
+                store = Store(
+                        name=str(shop).strip(),
+                        products=product
+                )
+                shops.append(store)
+            except KeyError:
+                continue
+        return shops
 
     def view_name(self):
         return self.__class__.__name__.lower()
 
     def view_attributes(self):
         return self.__dict__.keys()
-
-
-# protest = Product(1, 'foo', 2, ['bar'], ['ham'])
-#
-# print(f" name of class : {protest.view_name()}")
-# print(f"list of attributes : {protest.view_attributes()}")
-#
-# print(type(protest.__dict__.keys()))
-#
-# for i in protest.__dict__.keys():
-#     print(type(i))
-
